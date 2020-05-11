a4116600a589fcd3c50ea0c456dbfd9ea1a93c6f
izubenko <ihor.zubenko@limestonedigital.co>
2020-05-11 03:41:07 +0300

    remove comments from test_items file of lesson 369

--- test_items.py
+++ test_items.py
@@ -1,11 +1,10 @@
-
+import time
 #pytest -v --tb=line --reruns 1 --browser_name=chrome test_items.py
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_see_basket(browser):
+def test_see_add_basket_button(browser):
     browser.get(link)
     browser.implicitly_wait(5)
-    browser.find_element_by_css_selector('#add_to_basket_form > button')
-    #correct_text = browser.find_element_by_class_name('smart-hints__hint').text # считываем текст и записываем его в переменную
-    #assert "Correct!" == correct_text, f"Got: '{correct_text}', but should be 'Correct!'"
+    #time.sleep(30)
+    browser.find_element_by_css_selector('.btn-add-to-basket')
