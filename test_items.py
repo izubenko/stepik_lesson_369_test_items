591893e6f6984c8d4b479c75631d39bed21152a2
izubenko <ihor.zubenko@limestonedigital.co>
2020-05-11 04:00:30 +0300

    add asseration for test_items file of lesson 369

--- test_items.py
+++ test_items.py
@@ -1,5 +1,5 @@
+# pytest -v -s --tb=line --language=en test_items.py
 import time
-#pytest -v --tb=line --reruns 1 --browser_name=chrome test_items.py
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
@@ -7,4 +7,8 @@ def test_see_add_basket_button(browser):
     browser.get(link)
     browser.implicitly_wait(5)
     #time.sleep(30)
-    browser.find_element_by_css_selector('.btn-add-to-basket')
+    add_basket_btn = browser.find_elements_by_css_selector(".btn-add-to-basketw")
+    assert add_basket_btn, "Button not found!"
+
+    #assert #2
+    #assert len(browser.find_elements_by_css_selector(".btn-add-to-basket"))>0, "Button not found!"
